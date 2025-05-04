4cde359e61488b0b0dd2d2d2534df99c9327fafa
jeeva <periyasamyjeeva772@gmail.com>
2025-05-04 09:17:09 +0530

    init commit

--- src/main/java/com/example/practicejava/javapractice/JavapracticeApplication.java
+++ src/main/java/com/example/practicejava/javapractice/JavapracticeApplication.java
@@ -1,5 +1,6 @@
 package com.example.practicejava.javapractice;
 
+import com.example.practicejava.javapractice.problem16.Main16;
 import com.example.practicejava.javapractice.problem7.Main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +10,7 @@ public class JavapracticeApplication {
 
 	public static void main(String[] args) throws CloneNotSupportedException {
 		SpringApplication.run(JavapracticeApplication.class, args);
-		Main m=new Main();
+		Main16 m=new Main16();
 		m.executeAll();
 	}
 
